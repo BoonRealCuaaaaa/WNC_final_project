37be3a6fb896337a316c0235b3783d5039018fac
Hoang Nguyen <hiiback0608@gmail.com>
2024-12-09 21:11:38 +0700

    feat: UI for debit manager

---
+++ frontend/src/components/customer/debit-management/index.tsx
@@ -0,0 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/shared/tabs";
+import CreditorTable from "./creditor-table";
+import DebtorTable from "./debtor-table";
+
+export const DebitManagement = () => {
+   return (
+      <Tabs defaultValue="creditor">
+         <TabsList>
+            <TabsTrigger value="creditor">Do bạn tạo</TabsTrigger>
+            <TabsTrigger value="debtor">Người khác gửi</TabsTrigger>
+         </TabsList>
+         <TabsContent value="creditor">
+            <CreditorTable />
+         </TabsContent>
+         <TabsContent value="debtor">
+            <DebtorTable />
+         </TabsContent>
+      </Tabs>
+   );
+};
---
+++ frontend/src/pages/customer/debit-management/index.tsx
@@ -0,0 +1,7 @@
+import { DebitManagement } from "@/components/customer/debit-management";
+
+const DebitManagementPage = () => {
+   return <DebitManagement />;
+};
+
+export default DebitManagementPage;
