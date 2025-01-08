6d4bab9b4f1ea7ce02c1cc8333deb20482d74df8
Nguyen Trong Dai <ntd1241@gmail.com>
2025-01-08 17:12:50 +0700

    feat(beneficiary): complete api docs

--- backend/src/controllers/beneficiary.controller.js
+++ backend/src/controllers/beneficiary.controller.js
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import { models } from "../lib/utils/database/index.js";
 import { PGPSearchAccountApi } from "./interbank.controller.js";
 
@@ -37,7 +38,7 @@ export const createBeneficiary = async (req, res) => {
     return res.status(400).json({ message: "Không thể thêm bản thân" });
   } 
 
-  let receiver;
+  let receiver, name;
 
   if (bankName === process.env.BANK_NAME) {
     //Local
@@ -50,17 +51,29 @@ export const createBeneficiary = async (req, res) => {
   }
 
   if (!receiver) {
-    return res.status(404).json({ message: "Receiver not found" });
+    return res.status(404).json({ message: "Không tìm thấy người thụ hưởng" });
+  }
+
+  if (bankName === process.env.BANK_NAME) {
+    const receiverInfo = await models.Customer.findOne({
+      where: { id: receiver.customerId}
+    })
+    name = receiverInfo.fullName;
+  }
+  else {
+    //TODO: Add search name interbank
+    name = "???";
   }
 
   const beneficiaryEntity = await models.Beneficiaries.create({
+    name,
     shortName,
     bankName,
     accountNumber,
     customerId: customer.id,
   });
 
-  return res.status(201).json({ message: "Beneficiary created" });
+  return res.status(201).json({ message: "Thêm người thụ hưởng thành công" });
 };
 
 export const deleteBeneficiary = async (req, res) => {
@@ -81,12 +94,12 @@ export const deleteBeneficiary = async (req, res) => {
   });
 
   if (!beneficiary) {
-    return res.status(404).json({ message: "Beneficiary not found" });
+    return res.status(404).json({ message: "Không tìm thấy người thụ hưởng" });
   }
 
   beneficiary.destroy();
 
-  return res.status(200).json({ message: "Beneficiary deleted" });
+  return res.status(200).json({ message: "Xóa người thụ hưởng thành công" });
 };
 
 export const updateBeneficiary = async (req, res) => {
@@ -107,8 +120,29 @@ export const updateBeneficiary = async (req, res) => {
   });
 
   if (!beneficiary) {
-    return res.status(404).json({ message: "Beneficiary not found" });
+    return res.status(404).json({ message: "Không tìm thấy người thụ hưởng" });
   }
+
+  const beneficiaryTemp = await models.Beneficiaries.findOne({
+    where: { accountNumber, bankName },
+    include: {
+      model: models.Customer,
+      as: "customer",
+      where: { id: customer.id },
+    }
+  });
+
+  if (beneficiaryTemp && beneficiaryTemp.id !== id) {
+    return res
+      .status(400)
+      .json({ message: `${accountNumber} in ${bankName} bank is existed` });
+  }
+
+  const paymentAccount = await models.Paymentaccount.findOne({where: {customerId: customer.id}});
+
+  if (paymentAccount.accountNumber === accountNumber) {
+    return res.status(400).json({ message: "Không thể thêm bản thân" });
+  } 
 
   beneficiary.bankName = bankName;
   beneficiary.shortName = shortName;
@@ -116,5 +150,5 @@ export const updateBeneficiary = async (req, res) => {
 
   beneficiary.save();
 
-  return res.status(200).json({ message: "Beneficiary updated" });
+  return res.status(200).json({ message: "Thông tin người thụ hưởng đã được cập nhật" });
 };
--- backend/src/routes/beneficiary.route.js
+++ backend/src/routes/beneficiary.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import validateMiddleware from "../middlewares/ajv-validate.middleware.js";
 import { createBeneficiary, deleteBeneficiary, getBeneficiaries, updateBeneficiary } from "../controllers/beneficiary.controller.js";
-import { createBeneficiarySchema } from "../schemas/beneficiary.schema.js";
+import { createBeneficiarySchema, updateBeneficiarySchema } from "../schemas/beneficiary.schema.js";
 
 const router = express.Router();
 
@@ -13,7 +13,53 @@ const router = express.Router();
  *     description: API lấy danh sách người thụ hưởng của tài khoản khách hàng gọi tới
  *     responses:
  *       200:
- *         description: Lấy thành công
+ *         description: Thành công
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *             example:
+ *               - id: 1
+ *                 name: "John Snow"
+ *                 shortName: "Snow"
+ *                 bankName: "BANK A"
+ *                 accountNumber: "123413123"
+ *                 customerId: 1
+ *                 createdAt: "2025-01-08T10:24:18.000Z"
+ *                 updatedAt: "2025-01-08T10:24:18.000Z"
+ *     tags:
+ *       - Beneficiaries
+ */
+router.get("/", getBeneficiaries);
+
+/**
+ * @swagger
+ * /beneficiaries/:
+ *   post:
+ *     summary: Thêm người thụ hưởng
+ *     description: API thêm người thụ hưởng cho tài khoản khách hàng
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               shortName:
+ *                 type: string
+ *                 example: Chủ nợ
+ *                 description: Tên gợi nhớ
+ *               bankName:
+ *                 type: string
+ *                 example: MyBank
+ *                 description: Ngân hàng của người thụ hưởng
+ *               accountNumber:
+ *                 type: string
+ *                 example: 123456789012
+ *                 description: Ngân hàng của người thụ hưởng
+ *     responses:
+ *       201:
+ *         description: Thêm người thụ hưởng thành công
  *         content:
  *           application/json:
  *             schema:
@@ -21,15 +67,133 @@ const router = express.Router();
  *               properties:
  *                 message:
  *                   type: string
- *                   example: success
- *                 signature:
+ *                   example: Thêm người thụ hưởng thành công
+ *       400:
+ *         description: Thêm người thụ hưởng thất bại. Có thể là do người này đã thêm vào danh sách hoặc thông tin người thụ hưởng là bản thân.
+ *         content:
+ *           application/json:
+  *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
  *                   type: string
+ *                   example: Không thể thêm bản thân
+ *       404:
+ *         description: Không tìm thấy người thụ hưởng
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Không tìm thấy người thụ hưởng
  *     tags:
  *       - Beneficiaries
  */
-router.get("/", getBeneficiaries);
 router.post("/", validateMiddleware(createBeneficiarySchema),createBeneficiary);
+
+/**
+ * @swagger
+ * /beneficiaries/{id}:
+ *   delete:
+ *     summary: Xóa người thụ hưởng
+ *     description: API xóa người thụ hưởng khỏi tài khoản khách hàng
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         type: integer
+ *         required: true
+ *         description: ID người thụ hưởng lưu ở bảng Beneficiaries
+ *     responses:
+ *       200:
+ *         description: Xóa người thụ hưởng thành công
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Xóa người thụ hưởng thành công
+ *       404:
+ *         description: Xóa người thụ hưởng thất bại, có thể là do id người thụ hưởng không hợp lệ hoặc không nằm trong danh sách của tài khoản
+ *         content:
+ *           application/json:
+  *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Không tìm thấy người thụ hưởng
+ *     tags:
+ *       - Beneficiaries
+ */
 router.delete("/:id", deleteBeneficiary);
-router.put("/", updateBeneficiary);
+
+/**
+ * @swagger
+ * /beneficiaries/:
+ *   put:
+ *     summary: Sửa thông tin người thụ hưởng
+ *     description: API sửa thông tin người thụ hưởng của tài khoản khách hàng
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *                 description: ID của người thụ hưởng
+ *               shortName:
+ *                 type: string
+ *                 example: Chủ nợ
+ *                 description: Tên gợi nhớ
+ *               bankName:
+ *                 type: string
+ *                 example: MyBank
+ *                 description: Ngân hàng của người thụ hưởng
+ *               accountNumber:
+ *                 type: string
+ *                 example: 123456789012
+ *                 description: Ngân hàng của người thụ hưởng
+ *     responses:
+ *       200:
+ *         description: Thông tin người thụ hưởng đã được cập nhật
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Thông tin người thụ hưởng đã được cập nhật
+ *       400:
+ *         description: Sửa thông tin người thụ hưởng thất bại. Có thể là do thông tin sửa là của người đã thêm vào danh sách hoặc là bản thân.
+ *         content:
+ *           application/json:
+  *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Không thể thêm bản thân
+ *       404:
+ *         description: Không tìm thấy người thụ hưởng
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Không tìm thấy người thụ hưởng
+ *     tags:
+ *       - Beneficiaries
+ */
+router.put("/", validateMiddleware(updateBeneficiarySchema), updateBeneficiary);
 
 export default router;
