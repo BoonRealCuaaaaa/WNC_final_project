e8f10f0c7cb581f339bd6807cd55decd5de3a66a
Hoang Nguyen <99378331+BoonRealCuaaaaa@users.noreply.github.com>
2025-01-09 19:20:32 +0700

    Mock data (#11)
    * chore: add more data in mock-data.sql
    
    * chore: imporve dockerfile
    
    * chore: change BANK_NAME variable in docker-compose
    
    * chore(mock-data): change status and createdAt and updatedAt column in paymenttransaction data
    
    * chore: add more data in mock-data.sql
    
    * chore: imporve dockerfile
    
    * chore: change BANK_NAME variable in docker-compose
    
    * chore(mock-data): change status and createdAt and updatedAt column in paymenttransaction data
    
    * fix: missing MyBank in add beneficiary
    
    ---------
    
    Co-authored-by: ThienNhan2k3 <ntnhan21@clc.fitus.edu.vn>

--- frontend/src/components/customer/account-management/beneficiary-list/add-beneficiary-form/index.tsx
+++ frontend/src/components/customer/account-management/beneficiary-list/add-beneficiary-form/index.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Button, LoadingButton } from "@/components/ui/button"
+import { Button, LoadingButton } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -16,37 +16,41 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-import { DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { useMutation } from "@tanstack/react-query"
-import { getPartners } from "@/api/partner.api"
-import { useEffect, useState } from "react"
-import { toast } from "@/hooks/use-toast"
-import { createBeneficiaryApi } from "@/api/beneficiaries.api"
-import { AxiosError } from "axios"
-import { checkExistApi } from "@/api/customer.api"
-import { checkExistInterbankAccountsApi } from "@/api/interbank"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import {
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useMutation } from "@tanstack/react-query";
+import { getPartners } from "@/api/partner.api";
+import { useEffect, useState } from "react";
+import { toast } from "@/hooks/use-toast";
+import { createBeneficiaryApi } from "@/api/beneficiaries.api";
+import { AxiosError } from "axios";
+import { checkExistApi } from "@/api/customer.api";
+import { checkExistInterbankAccountsApi } from "@/api/interbank";
 
 const formSchema = z.object({
-    bankName: z.string()
-        .nonempty({
-            message: "Vui lòng chọn ngân hàng",
-        }),
-    accountNumber: z.string()
-        .nonempty({
-            message: "Vui lòng điền số tài khoản"
-        })
-        .regex(/^\d+$/, {
-            message: "Số tài khoản chỉ có thể chứa các chữ số",
-        }),
-    remindName: z.string(),
-})
+  bankName: z.string().nonempty({
+    message: "Vui lòng chọn ngân hàng",
+  }),
+  accountNumber: z
+    .string()
+    .nonempty({
+      message: "Vui lòng điền số tài khoản",
+    })
+    .regex(/^\d+$/, {
+      message: "Số tài khoản chỉ có thể chứa các chữ số",
+    }),
+  remindName: z.string(),
+});
 
 export default function AddBeneficiaryForm({
   onSuccess,
@@ -63,14 +67,18 @@ export default function AddBeneficiaryForm({
     },
   });
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        const { bankName, accountNumber, remindName } = values;
-        setIsSubmitting(true);
-        mutateCreateBeneficiary({ bankName, accountNumber, remindName: remindName !== "" ? remindName : recevier });
-    }
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const { bankName, accountNumber, remindName } = values;
+    setIsSubmitting(true);
+    mutateCreateBeneficiary({
+      bankName,
+      accountNumber,
+      remindName: remindName !== "" ? remindName : recevier,
+    });
+  }
 
   const [bankNames, setBankNames] = useState<string[]>([]);
 
@@ -92,58 +100,60 @@ export default function AddBeneficiaryForm({
     },
   });
 
-    useEffect(() => {
-        mutatePartners();
-    }, [mutatePartners])
+  useEffect(() => {
+    mutatePartners();
+  }, [mutatePartners]);
 
-    const [recevier, setReceiver] = useState<string | undefined>();
+  const [recevier, setReceiver] = useState<string | undefined>();
 
-    const { mutate: mutateReceiver } = useMutation({
-            mutationFn:  checkExistApi,
-            onSuccess: (response) => {
-                console.log(response.data);
-                if (response.status === 200) {
-                    setReceiver(response.data.fullName);
-                    form.clearErrors("accountNumber");
-                }
-            },
-            onError: () => {
-                setReceiver(undefined);
-                form.setValue("remindName", "");
-                form.setError("accountNumber", {
-                    type: "manual",
-                    message: "Không tìm thấy số tài khoản"
-                })
-            },
-        });
+  const { mutate: mutateReceiver } = useMutation({
+    mutationFn: checkExistApi,
+    onSuccess: (response) => {
+      console.log(response.data);
+      if (response.status === 200) {
+        setReceiver(response.data.fullName);
+        form.clearErrors("accountNumber");
+      }
+    },
+    onError: () => {
+      setReceiver(undefined);
+      form.setValue("remindName", "");
+      form.setError("accountNumber", {
+        type: "manual",
+        message: "Không tìm thấy số tài khoản",
+      });
+    },
+  });
 
-        const { mutate: mutateInterbankReceiver } = useMutation({
-            mutationFn:  checkExistInterbankAccountsApi,
-            onSuccess: (response) => {
-                console.log(response.data);
-                if (response.status === 200) {
-                    setReceiver(response.data.fullName);
-                    form.clearErrors("accountNumber");
-                }
-            },
-            onError: () => {
-                setReceiver(undefined);
-                form.setValue("remindName", "");
-                form.setError("accountNumber", {
-                    type: "manual",
-                    message: "Không tìm thấy số tài khoản"
-                })
-            },
-        });
+  const { mutate: mutateInterbankReceiver } = useMutation({
+    mutationFn: checkExistInterbankAccountsApi,
+    onSuccess: (response) => {
+      console.log(response.data);
+      if (response.status === 200) {
+        setReceiver(response.data.fullName);
+        form.clearErrors("accountNumber");
+      }
+    },
+    onError: () => {
+      setReceiver(undefined);
+      form.setValue("remindName", "");
+      form.setError("accountNumber", {
+        type: "manual",
+        message: "Không tìm thấy số tài khoản",
+      });
+    },
+  });
 
-    const handleMutateReceiver = () => {
-        if (import.meta.env.VITE_BANK_NAME === form.getValues("bankName")) {
-            mutateReceiver(form.getValues("accountNumber"))
-        }
-        else {
-            mutateInterbankReceiver({bankName: form.getValues("bankName"), accountNumber: form.getValues("accountNumber")})
-        }
+  const handleMutateReceiver = () => {
+    if (import.meta.env.VITE_BANK_NAME === form.getValues("bankName")) {
+      mutateReceiver(form.getValues("accountNumber"));
+    } else {
+      mutateInterbankReceiver({
+        bankName: form.getValues("bankName"),
+        accountNumber: form.getValues("accountNumber"),
+      });
     }
+  };
 
   const [submitStatus, setSubmitStatus] = useState<
     "default" | "submitting" | "failed" | "success"
@@ -153,105 +163,116 @@ export default function AddBeneficiaryForm({
     if (submitStatus === "success") onSuccess();
   }, [submitStatus]);
 
-    const { mutate: mutateCreateBeneficiary } = useMutation({
-        mutationFn: createBeneficiaryApi,
-        onSuccess: (response) => {
-            setIsSubmitting(false);
+  const { mutate: mutateCreateBeneficiary } = useMutation({
+    mutationFn: createBeneficiaryApi,
+    onSuccess: (response) => {
+      setIsSubmitting(false);
 
-            if (response.status === 201) {
-                setSubmitStatus("success");
-                toast({
-                    variant: "default",
-                    title: "Thêm người thụ hưởng thành công"
-                });
-            }
-        },
-        onError: (error: AxiosError) => {
-            setIsSubmitting(false);
-            setSubmitStatus("failed");
+      if (response.status === 201) {
+        setSubmitStatus("success");
+        toast({
+          variant: "default",
+          title: "Thêm người thụ hưởng thành công",
+        });
+      }
+    },
+    onError: (error: AxiosError) => {
+      setIsSubmitting(false);
+      setSubmitStatus("failed");
 
       const message = (error.response?.data as { message: string }).message;
 
-            toast({
-                variant: "destructive",
-                title: "Thêm người thụ hưởng thất bại",
-                description: message,
-            });
-        },
-    });
+      toast({
+        variant: "destructive",
+        title: "Thêm người thụ hưởng thất bại",
+        description: message,
+      });
+    },
+  });
 
-    const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const fullBankNames = [import.meta.env.VITE_BANK_NAME, ...bankNames];
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="dialog-form">
-                <DialogHeader>
-                    <DialogTitle>
-                        Thêm người thụ hưởng
-                    </DialogTitle>
-                </DialogHeader>
-                <FormMainContent>
-                    <FormHorizontalGroupFields>
-                        <FormField
-                            control={form.control}
-                            name="bankName"
-                            required
-                            render={({ field }) => (
-                                <FormItem className="max-w-[200px]">
-                                    <FormLabel>Ngân hàng</FormLabel>
-                                    <Select onValueChange={(event) => {
-                                        field.onChange(event);
-                                        handleMutateReceiver();
-                                    }} defaultValue={field.value}>
-                                        <FormControl>
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Chọn ngân hàng..." />
-                                            </SelectTrigger>
-                                        </FormControl>
-                                        <SelectContent>
-                                            {bankNames.map(bankName =>
-                                                <SelectItem value={bankName}>{bankName}</SelectItem>
-                                            )}
-                                        </SelectContent>
-                                    </Select>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="accountNumber"
-                            required
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Số tài khoản</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Nhập STK..." {...field} type="number" onBlur={handleMutateReceiver}/>
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </FormHorizontalGroupFields>
-                    <FormField
-                        control={form.control}
-                        name="remindName"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Tên gợi nhớ</FormLabel>
-                                <FormControl>
-                                    <Input placeholder={recevier ?? "Nhập tên gợi nhớ..."} {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="dialog-form">
+        <DialogHeader>
+          <DialogTitle>Thêm người thụ hưởng</DialogTitle>
+        </DialogHeader>
+        <FormMainContent>
+          <FormHorizontalGroupFields>
+            <FormField
+              control={form.control}
+              name="bankName"
+              required
+              render={({ field }) => (
+                <FormItem className="max-w-[200px]">
+                  <FormLabel>Ngân hàng</FormLabel>
+                  <Select
+                    onValueChange={(event) => {
+                      field.onChange(event);
+                      handleMutateReceiver();
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Chọn ngân hàng..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {fullBankNames.map((bankName) => (
+                        <SelectItem value={bankName}>{bankName}</SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="accountNumber"
+              required
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Số tài khoản</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nhập STK..."
+                      {...field}
+                      type="number"
+                      onBlur={handleMutateReceiver}
                     />
-                </FormMainContent>
-                <DialogFooter>
-                    <LoadingButton type="submit" isLoading={isSubmitting}>Thêm người thụ hưởng mới</LoadingButton>
-                </DialogFooter>
-
-            </form>
-        </Form>
-    )
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FormHorizontalGroupFields>
+          <FormField
+            control={form.control}
+            name="remindName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tên gợi nhớ</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder={recevier ?? "Nhập tên gợi nhớ..."}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormMainContent>
+        <DialogFooter>
+          <LoadingButton type="submit" isLoading={isSubmitting}>
+            Thêm người thụ hưởng mới
+          </LoadingButton>
+        </DialogFooter>
+      </form>
+    </Form>
+  );
 }
